2b10238b00ab7e3f789fa4dbbe3b14ef8f9b01bc
Michel Silva <michel.barbosa@hotmail.com>
2022-07-21 13:58:09 -0300

    build

---
+++ src/app/contato/contato.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+
+export class ContatoComponent implements OnInit {
+  formContato = this.formBuilder.group({
+    nome: ["", [
+      Validators.minLength(4),
+      Validators.required
+    ]],
+    assunto: ["", [
+      Validators.minLength(10),
+      Validators.required
+    ]],
+    telefone: ["", [
+      Validators.minLength(11),
+      Validators.required
+    ]],
+    email: ["", [
+      Validators.email,
+      Validators.required
+    ]],
+    mensagem: ["", [
+      Validators.minLength(50),
+      Validators.required
+    ]]
+});
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  enviarFormulario() {
+    alert("Sua mensagem foi enviada. Responderemos assim que possível!")
+    this.formContato.reset();
+  }
+}
---
+++ src/app/produtos.ts
@@ -0,0 +1,30 @@
+export interface IProduto {
+    id: number;
+    descricao: string;
+    preco: number;
+    descricaoPreco: string;
+    quantidade: number;
+    imagem: string;
+}
+
+export interface IProdutoCarrinho extends IProduto {
+    quantidade: number;
+}
+
+export const produtos: IProduto[] = [
+    { id: 1, descricao: "Macacão Azul Marinho", preco: 100.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 01.jpeg", quantidade: 10 },
+    { id: 2, descricao: "Conjunto Batizado", preco: 200.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 02.jpeg", quantidade: 10 },
+    { id: 3, descricao: "CJ Listrado c/ Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 03.jpeg", quantidade: 10 },
+    { id: 4, descricao: "CJ Vd e Bc c/ Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 04.jpeg", quantidade: 10 },
+    { id: 5, descricao: "Macacão Lilás", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 05.jpeg", quantidade: 10 },
+    { id: 6, descricao: "Macacão Azul Claro", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 06.jpeg", quantidade: 10 },
+    { id: 7, descricao: "CJ Vr e Bc c/ Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 07.jpeg", quantidade: 10 },
+    { id: 8, descricao: "CJ Lilás c/ Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 08.jpeg", quantidade: 10 },
+    { id: 9, descricao: "CJ Az e Rosa", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 09.jpeg", quantidade: 10 },
+    { id: 10, descricao: "CJ Az c/ Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 010.jpeg", quantidade: 10 },
+    { id: 11, descricao: "Macacão Salmão", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 011.jpeg", quantidade: 10 },
+    { id: 12, descricao: "CJ Palha com Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 012.jpeg", quantidade: 10 },
+    { id: 13, descricao: "Kit Az c/ Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 013.jpeg", quantidade: 10 },
+    { id: 14, descricao: "Kits Diversas Cores", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 014.jpeg", quantidade: 10 },
+    { id: 15, descricao: "CJ Vermelho c/ Manta", preco: 0.00, descricaoPreco: "Consultar condições", imagem: "./assets/Imagem 015.jpeg", quantidade: 10 }
+]
